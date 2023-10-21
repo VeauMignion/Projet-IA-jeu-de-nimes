4aa5c7310b6eb9aa4f355f11597d76532c38c4e7
VeauMignion <148464503+VeauMignion@users.noreply.github.com>
2023-10-21 13:06:44 +0200

    Create Interactions ondes.py

---
+++ Interactions ondes.py
@@ -0,0 +1,15 @@
+#créé par HUMBERT, le 21/10/2023
+#importations 
+import numpy  as np
+from matplotlib import pyplot as plt
+from math import exp, expm1, sqrt
+##le but de ce programme est de modéliser les zones où les interactions entre les ondes sont destructrices ou constructives, en fonction de:
+##le mode ondes sur l'eau:                                le mode lumière:
+##a la distance entre les sources des ondes (cm)          (micro m) 
+##lamb la longeur d'onde (cm)                             (nm)(400-800)
+##dT le déphasage temporel (micro s)                       =0
+##c la célérité de l'onde (m.s^-1)                         =300 000 000m.s^-1
+##D la distance par rapport à la source des ondes  (cm)    (m)
+##
+
+
